842f4a4b61dfd937c0a9ec33a36b408f1607bec3
randomdude999 <just.so.you.can.email.me@gmail.com>
2018-06-16 15:21:47 +0300

    Initial test of C# wrapper testing

---
+++ src/asar-tests/test.cs
@@ -0,0 +1,23 @@
+using AsarCLR;
+using System;
+using System.Reflection;
+
+class AsarTest
+{
+	static int Main(string[] args)
+	{
+		// TODO: actual tests here, right now i'm just figuring out C#
+		Asar.init();
+		Console.WriteLine("Asar version: {0}", Asar.version());
+		Console.WriteLine("Asar API version: {0}", Asar.apiversion());
+		// having some fun with reflection
+		// see guys, private fields are useless!
+		int expectedapiversion = (int)typeof(Asar).GetField("expectedapiversion", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+		Console.WriteLine("C# wrapper version: {0}", expectedapiversion);
+		if(Asar.apiversion() != expectedapiversion) {
+			Console.WriteLine("C# wrapper version and Asar API version don't match");
+			return 1;
+		}
+		return 0;
+	}
+}
